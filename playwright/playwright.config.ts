50de5b4334d927c360b75065082331183d8c14b6
BoronenkoTimurs <64987379+BoronenkoTimurs@users.noreply.github.com>
2024-12-01 17:45:16 +0200

    Setup job for testing.

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -14,12 +14,15 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // forbidOnly: !!process.env.CI,
+  // retries: process.env.CI ? 2 : 0,
+  // workers: process.env.CI ? 1 : undefined,
+  forbidOnly: true,
+  retries: 2,
+  workers: 1,
   reporter: 'html',
   use: {
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
   },
 
