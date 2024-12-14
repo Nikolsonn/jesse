acb0953017abfd1cb0d3df536c3e390ecdf59d31
BoronenkoTimurs <64987379+BoronenkoTimurs@users.noreply.github.com>
2024-12-14 23:01:19 +0200

    Add forgotten await.

--- playwright/tests/steps/components/navigation.ts
+++ playwright/tests/steps/components/navigation.ts
@@ -5,7 +5,7 @@ export class Navigation{
   constructor(public readonly page: Page){}
 
   public async openPage(pageId: string): Promise<void> {
-    test.step(`Open ${pageId} Space`, async () => {
+    await test.step(`Open ${pageId} Space`, async () => {
       await this.page.getByTestId(pageId).click({ force: true });
     });
   }
