1c9441f70dff9478b0a86b81f1f1c3defaaec4df
Nikolsonn <naleksankins@gmail.com>
2024-12-20 12:15:18 +0200

    Application is not building, revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 
 /*
-No need to use @rollup/plugin-commonjs - becauce vite use it by default: 
+No need to use @rollup/plugin-commonjs - becauce vite use it by default:
 https://github.com/vitejs/vite/discussions/11739#discussioncomment-4722002
 */
 
@@ -15,21 +15,20 @@ export default defineConfig({
     tsconfigPaths(),
     mkcert(),
   ],
-  build: {
-    rollupOptions: {
-      treeshake: false,
-      output: {
-        format: 'es',
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     treeshake: false,
+  //     output: {
+  //       format: 'es',
+  //     },
+  //   },
+  // },
   publicDir: './public',
   server: {
     host: true,
   },
-  define: {
-    'exports': {},
-    'process.env.PLAYWRIGHT_TEST': JSON.stringify(true),
-  },
+  // define: {
+  //   'exports': {},
+  //   'process.env.PLAYWRIGHT_TEST': JSON.stringify(true),
+  // },
 });
-
