ec3b7e4f5c140098530a70adab60275a4a634932
BoronenkoTimurs <64987379+BoronenkoTimurs@users.noreply.github.com>
2025-01-04 15:22:39 +0200

    Implement card open functionality for users.

--- src/pages/UserPage/AdBoardPage.tsx
+++ src/pages/UserPage/AdBoardPage.tsx
@@ -7,13 +7,53 @@ import {Page} from '@/components/Page.tsx';
 import {TEST_DATA} from "@/pages/TestDataUtils/testData.ts";
 
 export const AdBoardPage: FC = () => {
+    const [openCard, setOpenCard] = useState(null);
     const [files] = useState<File[]>();
 
-    const cardStyle = {
+    const cardTitle: CSSProperties = {
+    };
+    
+    const openCardTitle: CSSProperties = {
+        ...cardTitle,
+        overflow: 'scroll',
+        maxWidth: '260px',
+        maxHeight: '100%',
+        backgroundColor: '#000000',
+        borderRadius: '0 0 10px 10px',
+    };
+
+    const imgStyle: CSSProperties = {
+        width: '100%',
+        height: 'auto',
+        objectFit: 'cover',
+    };
+
+    const openImgStyle: CSSProperties = {
+        borderRadius: '10px 10px 0 0',
+    };
+
+    const cardStyle: CSSProperties = {
         width: 'calc(50% - 20px)', // 50% width minus margins
         margin: '10px',
         display: 'inline-block',
         verticalAlign: 'top',
+        transition: 'all 0.3s ease-in-out',
+    };
+
+    const openCardStyle: CSSProperties = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        borderRadius: '0',
+        background: 'rgba(0, 0, 0, 0.75)',
+        transition: 'all 0.3s ease-in-out',
+        zIndex: 1000,
     };
 
     const containerStyle: CSSProperties = {
@@ -36,16 +76,24 @@ export const AdBoardPage: FC = () => {
         navigate('/ad-request-page', {replace: true});
     };
 
+    const handleOpenCard = (key: string) => {
+        openCard === key ? setOpenCard(null) : setOpenCard(key);
+    };
+
     return (
         <Page back={true}>
             <div style={containerStyle}>
                 {TEST_DATA.map(items => (
-                    <Card key={items.imgSrc} style={cardStyle}>
+                    <Card 
+                        key={items.imgSrc} 
+                        style={openCard === items.imgSrc ? openCardStyle : cardStyle} 
+                        onClick={() => handleOpenCard(items.imgSrc)}
+                    >
                         <img
                             src={items.imgSrc}
-                            style={{width: '100%', height: 308, objectFit: 'cover'}}
+                            style={openCard === items.imgSrc ? openImgStyle : imgStyle}
                         />
-                        <Card.Cell>
+                        <Card.Cell style={openCard === items.imgSrc ? openCardTitle : cardTitle}>
                             <span>{items.title}</span>
                         </Card.Cell>
                     </Card>
